415da8ef5ba2e6938419fd15ea3427db73ce8f0e
hayatshin <hayat_shin@naver.com>
2024-08-30 11:48:27 +0900

    redirect

--- lib/router.dart
+++ lib/router.dart
@@ -5,6 +5,9 @@ import 'package:injicare_event/view/navigator_screen.dart';
 
 final router = GoRouter(
   initialLocation: "/",
+  redirect: (context, state) {
+    return null;
+  },
   routes: [
     GoRoute(
       name: "home",
