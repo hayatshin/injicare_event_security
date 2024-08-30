f594a9849683ae604584e198a0d6f2a159947d0d
hayatshin <hayat_shin@naver.com>
2024-08-30 11:57:21 +0900

    hash

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:injicare_event/constants/sizes.dart';
 import 'package:injicare_event/router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +17,7 @@ void main() async {
     anonKey: supabaseAnonKeyDebug!,
   );
 
-  setPathUrlStrategy();
+  // setPathUrlStrategy();
 
   runApp(
     const ProviderScope(
